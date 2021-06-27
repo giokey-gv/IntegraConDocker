f1733dae4eb26df956ab5315a2325a2df7f30f3d
JuanDiazCa <judiazca2@poligran.edu.co>
2021-06-27 02:12:17 -0500

    Add lint and test for Travis

--- pagina-angular/src/app/app.component.spec.ts
+++ pagina-angular/src/app/app.component.spec.ts
@@ -16,16 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pagina-angular'`, () => {
+  it(`should have as title 'Pagina Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pagina-angular');
+    expect(app.title).toEqual('Pagina Angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pagina-angular app is running!');
-  });
 });
